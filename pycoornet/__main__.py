8320e2cd330b1fad6984f1ca1e15b641f6073cbc
Camilo Soto Montoya <csotomon@hotmail.com>
2020-07-12 14:14:35 -0500

    reorganizacion

--- pycoornet/__main__.py
+++ pycoornet/__main__.py
@@ -1,14 +1,6 @@
-from .pycoornet import PyCooRnet
 from .app import App
 import pandas as pd
+from .crowdtangle import CrowdTangle
 
 if __name__ == '__main__':
     App.run()
-    # pc = PyCooRnet("test")
-    # datos = {
-    #     'url2': ['http://test.com','http://test2.com'],
-    #     'date2': [22000,25000]
-    #     }
-
-    # df = pd.DataFrame(datos)
-    # pc.get_crowtangle_shares(df, url_column='url2', date_column='date2')
--- pycoornet/app.py
+++ pycoornet/app.py
@@ -1,5 +1,14 @@
+from .crowdtangle import CrowdTangle
+import pandas as pd
+
 class App:
 
     @staticmethod
     def run():
         print("Hello World")
+
+    # @staticmethod
+    # def get_ct_shares():
+    #     urls_df = pd.read_csv('test_data.csv')
+    #     ct = CrowdTangle('hWGv30FDktGlUDKzjEV5ncoa3VMRptkfq2ChWmG7')
+    #     ct.get_shares(urls_df, date_column='publish_date')
