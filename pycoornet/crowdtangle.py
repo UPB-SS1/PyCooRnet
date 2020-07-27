2fb6f9e384a096e4bb9a490d363730f76500953b
Jose R. Zapata <jjrzg@hotmail.com>
2020-07-27 14:35:11 -0500

    change to google docstring

--- pycoornet/crowdtangle.py
+++ pycoornet/crowdtangle.py
@@ -24,27 +24,30 @@ class CrowdTangle:
             raise Exception('Crowdtangle Api Token is missing')
         self.api_key = api_key
 
-    def get_shares(self, urls, url_column='url', date_column='date', platforms=('facebook', 'instagram'), nmax=500, sleep_time=20, clean_urls=False, save_ctapi_output=False):
-        """Get the URLs shares from CrowdTangle.
+    def get_shares(self, urls, url_column='url', date_column='date', platforms=('facebook', 'instagram'), nmax=100, sleep_time=20, clean_urls=False, save_ctapi_output=False):
+        """ Get the URLs shares from CrowdTangle from a list of URLs with publish datetime
 
-        :param urls: A pandas dataframe with at least a column "url" containing the URLs, and a column "date" with their published date.
-        :type urls: pandas.DataFrame
-        :param url_column: Name of the column (placed inside quote marks) where the URLs are stored, defaults to 'url'
-        :type url_column: str, optional
-        :param date_column: name of the column (placed inside quote marks) where the date of the URLs are stored, defaults to 'date'
-        :type date_column: str, optional
-        :param platforms: a tuple of platforms to search. You can specify only facebook to search on Facebook, or only instagram to search on Instagram, defaults to ('facebook','instagram')
-        :type platforms: tuple, optional
-        :param nmax: max number of results for query, defaults to 500
-        :type nmax: int, optional
-        :param sleep_time: pause between queries to respect API rate limits. Default to 20 secs, it can be lowered or increased depending on the assigned API rate limit, defaults to 20
-        :type sleep_time: int, optional
-        :param clean_urls: clean the URLs from tracking parameters, defaults to False
-        :type clean_urls: bool, optional
-        :param save_ctapi_output: saves the original CT API output in ./rawdata/ct_shares.df.0.rds, defaults to False
-        :type save_ctapi_output: bool, optional
-        :return: A pandas dataframe of posts that shared the URLs and a number of variables returned by the https://github.com/CrowdTangle/API/wiki/Links CrowdTangle API links endpoint and the original data set of news.
-        :rtype: pandas.DataFrame
+        Args:
+            urls (dataframe): a dataframe with at least a column "url" containing the URLs, and a column "date" with their published date
+            url_column (str, optional): name of the column (placed inside quote marks) where the URLs are stored. Defaults to 'url'.
+            date_column (str, optional): name of the column (placed inside quote marks) where the date of the URLs are stored. Defaults to 'date'.
+            platforms (tuple, optional): a tuple of platforms to search. You can specify only facebook to search on Facebook, or only instagram to
+                                         search on Instagram. Defaults to ('facebook', 'instagram').
+            nmax (int, optional): max number of results for query. Defaults to 100.
+            sleep_time (int, optional): pause between queries to respect API rate limits. Default to 20 secs, it can be lowered or increased
+                                        depending on the assigned API rate limit. Defaults to 20.
+            clean_urls (bool, optional): clean the URLs from tracking parameters. Defaults to False.
+            save_ctapi_output (bool, optional): saves the original CT API output in ./rawdata/. Defaults to False.
+
+        Raises:
+            Exception: [description]
+            Exception: [description]
+            e: [description]
+            SystemExit: [description]
+
+        Returns:
+            [pandas.Dataframe]: [A pandas dataframe of posts that shared the URLs and a number of variables returned by the https://github.com/CrowdTangle/API/wiki/Links
+                    CrowdTangle API links endpoint and the original data set of news.
         """
 
         try:
