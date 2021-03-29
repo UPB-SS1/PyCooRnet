f554476c72e08dbff4e0a12efe40ac6fbde03e8c
Camilo Soto Montoya <csotomon@hotmail.com>
2021-03-29 10:09:51 -0500

    setup update

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pycoornet",
-    version="0.2.0",
+    version="0.2.1",
     description="Using Python Given a set of URLs, this packages detects coordinated link sharing behavior on social media and outputs the network of entities that performed such behaviour.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -39,6 +39,7 @@ setup(
           'PyCrowdTangle>=0.4.0',
           'tqdm>=4.47.0',
           'networkx>=2.4',
-          'python-louvain>=0.14'
+          'python-louvain>=0.14',
+          'tldextract>=3.1.0'
       ],
 )
