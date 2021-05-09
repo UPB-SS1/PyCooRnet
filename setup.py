a8987b74c3d233f703fd4991ae6957eca4109231
Jose R. Zapata <jjrzg@hotmail.com>
2021-05-09 16:57:45 -0500

    v0.3.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pycoornet",
-    version="0.2.5",
+    version="0.3.0",
     description="Using Python Given a set of URLs, this packages detects coordinated link sharing behavior on social media and outputs the network of entities that performed such behaviour.",
     long_description=long_description,
     long_description_content_type="text/markdown",
